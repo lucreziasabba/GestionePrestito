1e00776b1087ef01f0d5f7281b6bb056ff5950b8
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-03-04 13:18:20 +0100

    continuo wpf

--- WpfPrestito/MainWindow.xaml.cs
+++ WpfPrestito/MainWindow.xaml.cs
@@ -24,5 +24,30 @@ namespace WpfPrestito
         {
             InitializeComponent();
         }
+
+
+        public void Calcola_Click(object sender, RoutedEventArgs e)
+        {
+            double richiesta = double.Parse(txtImportoRichiesto.Text);
+            double rate = double.Parse(txtNRate.Text);
+            double percentuale = double.Parse(txtPercentuale.Text);
+            double interesse = (richiesta / 100) * percentuale;
+            double restituire = interesse + richiesta;
+            double per_rata = restituire / rate;
+            txtImportoDaRestituire.Text = ($"{restituire}");
+            txtImportoPerRata.Text = ($"{per_rata}");
+        }
+
+        public void Stampa_Click(object sender, RoutedEventArgs e)
+        {
+            string nome = txtNome.Text;
+            string cognome = txtCognome.Text;
+            string data = dataPicker;
+            string luogo = ComboBoxItem;
+            if (Femmina.IsChecked == true )
+                lblStampato.Content = ($"{cognome} {nome} residente in {} nata il {}. Prestito di {txtImportoRichiesto}€ ad un tasso del {txtPercentuale}% da restituire in {txtNRate} da {txtImportoPerRata}€ ciascuna, per un totale di {txtImportoDaRestituire}€");
+            else if (Maschio.IsChecked == true)
+                lblStampato.Content = ($"{cognome} {nome} residente in {} nato il {}. Prestito di {txtImportoRichiesto}€ ad un tasso del {txtPercentuale}% da restituire in {txtNRate} da {txtImportoPerRata}€ ciascuna, per un totale di {txtImportoDaRestituire}€");
+        }
     }
 }
